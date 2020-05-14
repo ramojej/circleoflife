c69da3e76e9afe299cad5d2d5a1ea3966b4ff25c
Jejo <jejomardorongon@gmail.com>
2020-05-14 23:22:38 +0800

    Fix some hovering issues in categories

--- src/templates/CategoryTemplate.js
+++ src/templates/CategoryTemplate.js
@@ -8,7 +8,29 @@ const CategoryTemplate = props => {
 
   const { pathContext, data } = props
 
-  return <div>from {pathContext.category} </div>
+  return (
+    <Layout>
+      <Banner
+        title={
+          pathContext.category === "General"
+            ? "Anything under the sun"
+            : "Articles about " + pathContext.category
+        }
+        img={data.bannerImg.childImageSharp.fluid}
+      />
+      <div>
+        <div>
+          {data.blogPosts.edges.map(({ node }, index) => {
+            return (
+              <div key={index}>
+                <h2>{node.title}</h2>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export const query = graphql`
@@ -21,6 +43,16 @@ export const query = graphql`
           createdAt(formatString: "MMM DD, YYYY")
           timeCreated: createdAt(formatString: "hh:mm a")
           title
+          shortDesc {
+            shortDesc
+          }
+        }
+      }
+    }
+    bannerImg: file(relativePath: { eq: "blog-banner.jpg" }) {
+      childImageSharp {
+        fluid(quality: 100, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
         }
       }
     }
