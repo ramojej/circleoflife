3610f55e23adb57ad8b86e643bdf1c765f474e64
Jejo <jejomardorongon@gmail.com>
2020-05-21 22:03:37 +0800

    Refactor contact css and setup contact details

---
+++ src/components/Contact/ContactDetails.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { FaEnvelope, FaPhone } from "react-icons/fa"
+
+const ContactDetails = () => {
+  return <div></div>
+}
+
+export default ContactDetails
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,13 +3,34 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import Banner from "../sections/Banner"
 import ContactForm from "../components/Contact/ContactForm"
+import styles from "../css/contact.module.css"
 
 const contact = props => {
   const { data } = props
   return (
     <Layout>
       <Banner title="Contact Us" img={data.bannerImg.childImageSharp.fluid} />
-      <ContactForm />
+      <div className="main-cont">
+        <div className={styles.sectionOne}>
+          <div className={styles.formContainer}>
+            <h2>Send us a message</h2>
+            <ContactForm />
+          </div>
+          <div>
+            <h2 className="mt-8 text-2xl font-bold">Get in touch</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+              ipsum nemo non magni consequatur cupiditate dolor provident ipsam,
+              eligendi id?
+            </p>
+            <p>
+              Debitis ducimus voluptas aliquid corrupti, perferendis tenetur
+              nesciunt nam neque esse itaque numquam minima, ex aut. Facilis,
+              soluta! Culpa, doloribus.
+            </p>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
