ba4b0be33af6cac9e9cd9e6c7b94663e7a36e980
Jejo <jejomardorongon@gmail.com>
2020-05-29 18:33:46 +0800

    Removed animation and compressed some images because it affected site speed

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -1,21 +1,21 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { animated, useTransition } from "react-spring"
+//import { animated, useTransition } from "react-spring"
 import { BsMoon, BsSun } from "react-icons/bs"
 
 const ThemeSwitcher = props => {
-  const transitions = useTransition(props.isDarkMode, null, {
-    from: item => ({
-      position: "absolute",
-      opacity: 0,
-      transform: item === false ? "translateY(50px)" : "translateY(-50px)",
-    }),
-    enter: { opacity: 1, transform: "translateY(0)" },
-    leave: item => ({
-      opacity: 0,
-      transform: item === false ? "translateY(50px)" : "translateY(-50px)",
-    }),
-  })
+  // const transitions = useTransition(props.isDarkMode, null, {
+  //   from: item => ({
+  //     position: "absolute",
+  //     opacity: 0,
+  //     transform: item === false ? "translateY(50px)" : "translateY(-50px)",
+  //   }),
+  //   enter: { opacity: 1, transform: "translateY(0)" },
+  //   leave: item => ({
+  //     opacity: 0,
+  //     transform: item === false ? "translateY(50px)" : "translateY(-50px)",
+  //   }),
+  // })
   //console.log(transitions)
   return (
     <button
@@ -24,8 +24,8 @@ const ThemeSwitcher = props => {
       aria-label="Theme Switcher"
       className={`focus:outline-none sm:ml-5 mr-8 inline-flex items-center bg-transparent border-none cursor-pointer relative text-yellow-500 ${props.className}`}
     >
-      {/* {props.isDarkMode ? <BsMoon /> : <BsSun className="text-yellow-500" />} */}
-      {transitions.map(({ item, key, props }) =>
+      {props.isDarkMode ? <BsMoon /> : <BsSun className="text-yellow-500" />}
+      {/* {transitions.map(({ item, key, props }) =>
         item === true ? (
           <animated.span style={props} key={key}>
             <BsMoon />
@@ -35,7 +35,7 @@ const ThemeSwitcher = props => {
             <BsSun />
           </animated.span>
         )
-      )}
+      )} */}
     </button>
   )
 }
